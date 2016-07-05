0f9a4e95df992ac8b60c3674773fc93a716f1d5e
varunpurohit76 <varunpurohit76@gmail.com>
2016-07-06 01:28:47 +0530

    basic game up and running

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 import pygame
 import time
+import random
 
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+    textSurface = font.render(text, True, red)
     return textSurface, textSurface.get_rect()
+
+def obstacle(obstacle_x, obstacle_y, obstacle_w, obstacle_h, obstacle_c):
+    pygame.draw.rect(gameDisplay, obstacle_c, [obstacle_x, obstacle_y, obstacle_w, obstacle_h])
 
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',50)
@@ -17,14 +21,27 @@ def message_display(text):
     time.sleep(2)
     game_loop()
 
+def display_score(score):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Score : "+str(score), True, red)
+    gameDisplay.blit(text,(0,0))
+
 def crash():
-    message_display('You touched the walls')
+    message_display('You crashed bruh!')
 
 def game_loop():
     x = display_width * 0.5
     y = display_height * 0.5
     dx = 0
     dy = 0
+    
+    obstacle_x = random.randrange(0, display_width)
+    obstacle_y = -600
+    obstacle_speed = 5
+    obstacle_w = 100
+    obstacle_h = 100
+    score_start_time = time.time()   
+
     exit_game = False
 
     while not exit_game:
@@ -61,10 +78,22 @@ def game_loop():
             y = 600 - obj_height
 
         gameDisplay.fill(white)
+            
+        obstacle(obstacle_x, obstacle_y, obstacle_w, obstacle_h, black)
+        obstacle_y += obstacle_speed
+
+        if obstacle_y >= display_height:
+            obstacle_y = 0 - obstacle_h
+            obstacle_x = random.randrange(0, display_width)
+            obstacle_speed += 2
+            obstacle_w *= 1.2
+
         car(x, y)
+        display_score("%0.1f" % (time.time() - score_start_time))
         
-        if x == 0 or x == (800 - obj_width):
-            crash()
+        if y < (obstacle_y + obstacle_h) and (y + obj_height) > obstacle_y:
+            if x > obstacle_x and x < obstacle_x + obstacle_w or x + obj_width > obstacle_x and x + obj_width < obstacle_x + obstacle_w:
+                crash()
 
         pygame.display.update()
         clock.tick(60)
@@ -72,7 +101,7 @@ def game_loop():
 pygame.init()
 display_width = 800
 display_height = 600
-obj_height = 49
+obj_height = 50
 obj_width = 50
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Game')
@@ -80,7 +109,7 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 clock = pygame.time.Clock()
-carImg = pygame.image.load('ball.png')
+carImg = pygame.image.load('obj.png')
 game_loop()
 pygame.quit()
 quit()
